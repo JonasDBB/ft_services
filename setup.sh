1cac3c61cbae0e8ed9258acc1b4015c8e6558ca7
Jonas <jbennink@student.codam.nl>
2020-11-13 14:28:23 +0100

    yep

--- setup.sh
+++ setup.sh
@@ -5,19 +5,29 @@ minikube addons enable metrics-server
 minikube addons enable dashboard
 eval $(minikube docker-env)
 
+# applying metallb yaml
 kubectl apply -f srcs/metallb.yaml
 
-# build services
+# build images and services
+# nginx
 docker build -t nginx srcs/nginx/
 kubectl apply -f srcs/nginx.yaml
 
+# mysql
 docker build -t mysql srcs/mysql/
 kubectl apply -f srcs/mysql.yaml
 
+# wordperss . Yes. wordperss
 docker build -t wordpress srcs/wordpress/
 kubectl apply -f srcs/wordpress.yaml
 
+# phpmyadmin
 docker build -t phpmyadmin srcs/phpmyadmin/
 kubectl apply -f srcs/phpmyadmin.yaml
 
+# ftps
+docker build -t ftps srcs/ftps/
+kubectl apply -f srcs/ftps.yaml
+
+# running dashboard (pray to the kubergods it works)
 minikube dashboard
